c35dcc86d9951b1b66ade07fb4a69e0d588fff40
mouravievt <mouravievt@wit.edu>
2020-07-21 19:50:45 -0400

    Added the tabs component.

--- react-app/src/containers/Body.js
+++ react-app/src/containers/Body.js
@@ -1,10 +1,21 @@
 import React from "react";
 import './Containers.css';
-import classes from './Containers.css';
+import {Tabs, Tab} from 'react-bootstrap';
+
 const body = () => {
     return (
         <div>
-            Body
+            <Tabs defaultActiveKey="chatbot" id="doc-bot-body-tabs">
+                <Tab eventKey="chatbot" title="Watson AI Chatbot">
+                    first
+                </Tab>
+                <Tab eventKey="resources" title="At Home Resources">
+                    second
+                </Tab>
+                <Tab eventKey="contacts" title="Emergency Contact List" >
+                    third
+                </Tab>
+            </Tabs>
         </div>
     )
 };
