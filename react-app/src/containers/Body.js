624e66219140ec29a51dc73eb435ef2dc9563e58
mouravievt <mouravievt@wit.edu>
2020-07-23 19:59:59 -0400

    Implemented the Watson AI Assistant

--- react-app/src/containers/Body.js
+++ react-app/src/containers/Body.js
@@ -5,10 +5,7 @@ import {Tabs, Tab} from 'react-bootstrap';
 const body = () => {
     return (
         <div>
-            <Tabs defaultActiveKey="chatbot" id="doc-bot-body-tabs">
-                <Tab eventKey="chatbot" title="Watson AI Chatbot">
-                    first
-                </Tab>
+            <Tabs defaultActiveKey="resources" id="doc-bot-body-tabs">
                 <Tab eventKey="resources" title="At Home Resources">
                     second
                 </Tab>
